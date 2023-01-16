ddac502aefc3cb7fadf231e8605253886bd0b37b
Kyle Groening <kylegroening@gmail.com>
2023-01-15 19:08:01 -0500

    Move merge-sort into separate file

--- recusion.js
+++ recusion.js
@@ -43,34 +43,3 @@ function fibsRec (number){
     return arr
   }
 }
-
-// merge sort top level, uses mergeInOrder(x, y)
-function mergeSort(array){
-  if (array.length < 2){
-    return array
-  } else {
-    let x = mergeSort(array.slice(0, Math.floor(array.length/2)))
-    let y = mergeSort(array.slice(Math.floor(array.length/2)))
-    const arr = mergeInOrder(x, y)
-    return arr
-  }
-}
-
-// merge in order
-function mergeInOrder(x, y){
-  let arr = []
-  while(x.length > 0 && y.length > 0){
-    if (x[0] < y[0]){
-      arr.push(x.shift()) 
-    } else {
-      arr.push(y.shift())
-    }
-  }
-  while (x.length > 0){
-    arr.push(x.shift())
-  }
-  while (y.length > 0){
-    arr.push(y.shift())
-  }
-  return arr
-}
